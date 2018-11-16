0dd764dfb69f4966673dacbf4c448d41b7e18562
twisti <ladent.thibault@gmail.com>
2018-11-16 23:53:24 +0100

    verif des autorisations  pour acceder a la localisation

--- app/src/main/java/com/ladent/tp/geolocalisation/MainActivity.kt
+++ app/src/main/java/com/ladent/tp/geolocalisation/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.ladent.tp.geolocalisation
 
-import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
@@ -9,80 +8,83 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.activity_main.*
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.LocationManager
+import android.provider.Settings
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
+    //l'objet provenant de l'api google, qui va nous permettre de recuperer les dernieres localisations de l'utilisateur
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+
+
+    // Il faut choisir la meme permission que celle choisi dans le manifest.
+    //  private val permissionLocation = Manifest.permission.ACCESS_FINE_LOCATION
+    private val permissionLocation = Manifest.permission.ACCESS_COARSE_LOCATION
+
+
+
+    lateinit var locationManager: LocationManager
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //l'objet provenant de l'api google, qui va nous permettre de recuperer les dernieres localisations de l'utilisateur
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        val geocoder = Geocoder(this, Locale.getDefault())
+         val geocoder = Geocoder(this, Locale.getDefault())
 
 
         buttonLastLoca.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)== android.content.pm.PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this,
+                    this.permissionLocation)!= PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(this.permissionLocation), 1)
+            }else{
+
                 buttonLastLoca.setOnClickListener {
-                    fusedLocationClient.lastLocation
-                        .addOnSuccessListener { location: Location? ->
-                            if (location != null) {
-                                var message = "long " + location.longitude + " -  lat " + location.latitude
-                                textView.text = message
+                    if (!isLocationEnabled()){
+                        val viewIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                        startActivity(viewIntent)
+                    }else {
+                        fusedLocationClient.lastLocation
+                            .addOnSuccessListener { location: Location? ->
+                                if (location != null) {
+                                    var message = "long " + location.longitude + " -  lat " + location.latitude
+                                    textView.text = message
 
-
-                                var addresses = geocoder.getFromLocation( location.latitude,location.longitude,1)
-
-                                var fullAddress = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-
-
-                                textAdress.text = fullAddress
-
-
-                            } else {
-                                Toast.makeText(this, "no location found", Toast.LENGTH_LONG).show()
+                                     var addresses = geocoder.getFromLocation( location.latitude,location.longitude,1)
+                                     textAdress.text = addresses.get(0).getAddressLine(0)
+                                } else {
+                                    Toast.makeText(this, "no location found", Toast.LENGTH_LONG).show()
+                                }
                             }
-                        }
+                    }
                 }
-            } else {
-                askForPermission()
             }
         }
     }
 
 
-    private fun askForPermission() {
-        // Permission is not granted
-        // Should we show an explanation?
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        ) {
-            // Show an explanation to the user *asynchronously* -- don't block
-            // this thread waiting for the user's response! After the user
-            // sees the explanation, try again to request the permission.
-        } else {
-            // No explanation needed, we can request the permission.
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                1
-            )
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
+    private fun isPermissionEnable() : Boolean{
+        return ContextCompat.checkSelfPermission(this,
+            this.permissionLocation)== PackageManager.PERMISSION_GRANTED
         }
+
+    private fun isLocationEnabled(): Boolean {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 
 
-
 }
-
 
 
