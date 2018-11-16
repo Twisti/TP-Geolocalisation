af9ee7ee289c95c76e473e2feb7369252522a10d
twisti <ladent.thibault@gmail.com>
2018-11-16 13:36:38 +0100

    modify build.gredle

--- app/src/main/java/com/ladent/tp/geolocalisation/MainActivity.kt
+++ app/src/main/java/com/ladent/tp/geolocalisation/MainActivity.kt
@@ -1,12 +1,88 @@
 package com.ladent.tp.geolocalisation
 
-import android.support.v7.app.AppCompatActivity
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.android.gms.location.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val geocoder = Geocoder(this, Locale.getDefault())
+
+
+        buttonLastLoca.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)== android.content.pm.PackageManager.PERMISSION_GRANTED){
+                buttonLastLoca.setOnClickListener {
+                    fusedLocationClient.lastLocation
+                        .addOnSuccessListener { location: Location? ->
+                            if (location != null) {
+                                var message = "long " + location.longitude + " -  lat " + location.latitude
+                                textView.text = message
+
+
+                                var addresses = geocoder.getFromLocation( location.latitude,location.longitude,1)
+
+                                var fullAddress = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+
+
+                                textAdress.text = fullAddress
+
+
+                            } else {
+                                Toast.makeText(this, "no location found", Toast.LENGTH_LONG).show()
+                            }
+                        }
+                }
+            } else {
+                askForPermission()
+            }
+        }
     }
+
+
+    private fun askForPermission() {
+        // Permission is not granted
+        // Should we show an explanation?
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            // Show an explanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+        } else {
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
+    }
+
+
+
 }
+
+
+
